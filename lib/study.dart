5e596c515ed57f848b1180e0e9182199fca615cd
emircaglar <emircaglar2906@gmail.com>
2021-10-04 21:10:47 +0200

    campus studycontainer

--- lib/study.dart
+++ lib/study.dart
@@ -26,11 +26,11 @@ class Study extends StatelessWidget {
           Expanded(
             child: SingleChildScrollView(
               child: Column(
-                //mainAxisAlignment: MainAxisAlignment.spaceAround,
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Container(
                     child: ExpansionTile(
-                      textColor:Colors.black,
+                      textColor: Colors.black,
                       trailing: Text(""),
                       title: Text(
                         "Kursen",
@@ -42,121 +42,87 @@ class Study extends StatelessWidget {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            HomeUser(media: media,color:Colors.grey,title:"ali"),
-                            HomeUser(media: media,color:Colors.grey,title:"ali"),
+                            HomeUser(
+                                media: media, color: Colors.grey, title: "ali"),
+                            HomeUser(
+                                media: media, color: Colors.grey, title: "ali"),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            HomeUser(media: media,color:Colors.grey,title:"ali"),
-                            HomeUser(media: media,color:Colors.grey,title:"ali"),
+                            HomeUser(
+                                media: media, color: Colors.grey, title: "ali"),
+                            HomeUser(
+                                media: media, color: Colors.grey, title: "ali"),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            HomeUser(media: media,color:Colors.grey,title:"ali"),
-                            HomeUser(media: media,color:Colors.grey,title:"ali"),
+                            HomeUser(
+                                media: media, color: Colors.grey, title: "ali"),
+                            HomeUser(
+                                media: media, color: Colors.grey, title: "ali"),
                           ],
                         ),
                       ],
                     ),
                     padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 24),
-                    margin: EdgeInsets.symmetric(vertical: 8,horizontal: 8),
+                        horizontal: 6.0, vertical: 24),
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         color: Colors.blue[100]),
                     width: media.width * .9,
                   ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 24),
-                    margin: EdgeInsets.only(bottom: 8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.blue[100]),
-                    width: media.width * .9,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Über uns",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        Text(
-                            "Information über the projectInformation about the project Information about the project Information about the project"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 24),
-                    margin: EdgeInsets.only(bottom: 8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.blue[100]),
-                    width: media.width * .9,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "About us",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        Text(
-                            "Information about the project Information about the project Information about the project"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 24),
-                    margin: EdgeInsets.only(bottom: 8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.blue[100]),
-                    width: media.width * .9,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "About us",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        Text(
-                            "Information about the project Information about the project Information about the project"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 24),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.blue[100]),
-                    width: media.width * .9,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "About us",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        Text(
-                            "Information about the project Information about the project Information about the project"),
-                      ],
-                    ),
-                  ),
+                  StudyContainer(media: media,title:"About us", zweitetitle:"Letzte Dialog Information about the project Information about the project Information about the project"),
+                  StudyContainer(media: media,title:"About us",zweitetitle:"Letzte Dialog Information about the project Information about the project Information about the project"),
+                  StudyContainer(media: media,title:"About us",zweitetitle:"Letzte Dialog Information about the project Information about the project Information about the project"),
+
                 ],
               ),
             ),
           ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class StudyContainer extends StatelessWidget {
+  const StudyContainer({
+    Key? key,
+    required this.media,
+    required this.title,
+    required this.zweitetitle,
+  }) : super(key: key);
+
+  final Size media;
+  final String title;
+  final String zweitetitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Colors.blue[100]),
+      width: media.width * .9,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 24),
+          ),
+          Divider(),
+
+          Text(
+              zweitetitle),
         ],
       ),
     );
